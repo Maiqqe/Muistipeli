403e8a448b156148bec5094587acc06fdc527220
Maiqqe <maisa.tuomenpuro@student.hamk.fi>
2023-10-18 18:48:05 +0300

    Tiedoston luonti ja tervehdys

---
+++ src/Muistipeli.java
@@ -0,0 +1,14 @@
+
+public class Muistipeli {
+
+	public static void main(String[] args) {
+		tervehdys();
+
+	}//Mainin loppu
+	
+	public static void tervehdys ()
+	{	System.out.println("Tervetuloa pelaamaan Muistipeliä!");
+		System.out.println("Yritä muistaa alla olevat 7 numeroa väliltä 1-5. Näet numerot vain hetken.");
+	}
+	
+}//Vimppa
