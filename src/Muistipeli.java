20a9b1714fce845897027c82186e95842e0878b1
Maiqqe <maisa.tuomenpuro@student.hamk.fi>
2023-10-18 18:50:17 +0300

    Arvotut numerot lisäys

--- src/Muistipeli.java
+++ src/Muistipeli.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Muistipeli {
 
 	public static void main(String[] args) {
 		tervehdys();
+		arvotutNumerot ();
 
 	}//Mainin loppu
 	
@@ -11,4 +14,16 @@ public class Muistipeli {
 		System.out.println("Yritä muistaa alla olevat 7 numeroa väliltä 1-5. Näet numerot vain hetken.");
 	}
 	
+	public static ArrayList<Integer> arvotutNumerot ()
+	{	Random random = new Random(); // Numeroiden arpoja			
+		ArrayList<Integer> arvotut = new ArrayList <Integer>(); //Yksiulotteinen taulukko arvotuille numeroille
+	
+			for (int i = 0; i < 7; i++)
+			{int luvut= random.nextInt(5)+1; 
+			arvotut.add(luvut);
+			}
+
+			System.out.println(arvotut);
+			return arvotut; //Palauttaa arvotut numerot
+	}
 }//Vimppa
